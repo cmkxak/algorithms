604807077977632c27ed8310d04d12bcb7594b92
cmkxak <gcm466@naver.com>
2022-01-10 17:54:50 +0900

    [이코테]Greedy 문제 추가

---
+++ Greedy/큰수의 법칙.py
@@ -0,0 +1,21 @@
+n,m,k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+
+result = 0
+first = data[n-1]
+second = data[n-2]
+
+while True:
+  for i in range(k):
+    if m == 0:
+      break
+    result+=first
+    m-=1
+  
+  if m == 0:
+    break
+  result+=second
+  m-=1
+print(result)
