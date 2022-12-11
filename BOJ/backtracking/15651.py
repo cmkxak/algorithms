44c1826cdc340de2830d20169e306c8c3e41e9fe
cmkxak <gcm466@naver.com>
2022-12-12 00:33:10 +0900

    [백준]15651,15652 불필요한 로직 제거

--- BOJ/backtracking/15651.py
+++ BOJ/backtracking/15651.py
@@ -1,17 +1,11 @@
 n,m = map(int, input().split())
-visited = [False] * (n+1)
 nums = []
-
 def dfs():
-
     if len(nums) == m:
         print(*nums)
         return
-
     for i in range(1,n+1):
-        visited[i] = True
         nums.append(i)
         dfs()
         nums.pop()
-        visited[i] = False
 dfs()
--- BOJ/backtracking/15652.py
+++ BOJ/backtracking/15652.py
@@ -1,5 +1,4 @@
 n,m = map(int, input().split())
-visited = [False] * (n+1)
 nums = []
 
 def dfs(start):
@@ -8,9 +7,7 @@ def dfs(start):
         return
 
     for i in range(start,n+1):
-        visited[i]=True
         nums.append(i)
         dfs(i)
         nums.pop()
-        visited[i] = False
 dfs(1)
