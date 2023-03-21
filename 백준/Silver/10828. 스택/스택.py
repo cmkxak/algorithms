4cbd367aafb3e89cbd33344ef720835f27eaabe6
CHULMIN KWAK <gcm466@naver.com>
2023-03-22 00:59:32 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,24 @@
+import sys
+n = int(sys.stdin.readline())
+stack = []
+for _ in range(n):
+  command = sys.stdin.readline().split()
+  if command[0] == "push":
+    stack.append(int(command[1]))
+  elif command[0] == "pop":
+    if stack:
+        print(stack.pop())
+    else:
+        print(-1)
+  elif command[0] == "size":
+    print(len(stack))
+  elif command[0] == "empty":
+    if stack:
+        print(0)
+    else:
+        print(1)
+  elif command[0] == "top":
+    if not stack:
+        print(-1)
+    else:
+        print(stack[-1])
