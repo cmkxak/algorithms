cca5198dc05274bb397bb448a89b5346bfb07554
Chulmin Kwak <gcm466@naver.com>
2023-04-12 11:13:17 +0900

    [Gold V] Title: 치킨 배달, Time: 396 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,27 +1,33 @@
 from itertools import combinations
 
-n,m=map(int, input().split())
-chicken, house= [],[]
+def check():
+    result = 1e9
+    for c in chicken_house:
+        total_chicken_dist = 0
+        for x,y in homes:
+            each_dist= 1e9
+            for j in range(m):
+                #각 집의 치킨 거리
+                each_dist = min(each_dist, abs(c[j][0] - x) + abs(c[j][1] - y))
+            #도시의 치킨 거리
+            total_chicken_dist += each_dist
+        result = min(result, total_chicken_dist)
+    return result
 
-for r in range(n):
-    data=list(map(int, input().split()))
-    for c in range(n):
-        if data[c] == 1:
-            house.append((r,c)) #일반 집
-        elif data[c] == 2:
-            chicken.append((r,c)) #치킨 집
+n,m =map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
 
-candidates = list(combinations(chicken, m)) #치킨집이 m개만 남는 경우의 수
-
-result = 1e9
-for c in candidates:
-    dist = 0
-    tmp = 0
-    for hx, hy in house:
-        res= 1e9
-        for j in range(m):
-            #남는 치킨집의 조합에서 모든 집과 치킨집간의 치킨 거리를 구하여 최솟값을 구한다.
-            res = min(res, abs(c[j][0]-hx) + abs(c[j][1] - hy)) #치킨 거리 계산
-        dist+=res #모든 집의 치킨 거리 
-    result = min(result, dist)
-print(result)
+#치킨집
+chickens = []
+homes = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 2:
+            chickens.append((i,j))
+        #집인 경우
+        if graph[i][j] == 1:
+            homes.append((i,j))
+chicken_house = list(combinations(chickens, m))
+print(check())
