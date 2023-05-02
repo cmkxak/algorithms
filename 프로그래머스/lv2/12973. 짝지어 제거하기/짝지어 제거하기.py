5bee059b208db30056f71a73f2003e5955432e7a
Chulmin Kwak <gcm466@naver.com>
2023-05-02 10:27:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 94.43 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if not stack:
+            stack.append(i)
+        else:
+            if i == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(i)
+    if stack:
+        return 0
+    else:
+        return 1
