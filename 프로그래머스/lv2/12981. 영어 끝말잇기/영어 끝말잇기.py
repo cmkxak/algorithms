d1e237c16753bb576f69a3f3aac62d819aa28877
Chulmin Kwak <gcm466@naver.com>
2023-05-02 23:22:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,9 +1,18 @@
 def solution(n, words):
     answer = []
+    level = []
+    for i in range(0,len(words),n):
+        level.append(words[i:n + i])
+    print(level)
+    #탈락 조건
+    #이전 단어의 마지막 != 다음 시작 단어
     
-    for i in range(1, len(words)):
-        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
-            answer.append([i % n +1, i//n + 1])
-    else:
-        answer.append([0,0])
-    return answer[0]
+    for idx, l in enumerate(level):
+        for j in range(1,len(l)): 
+            if level[j-1][-1] != level[j][0]:
+                answer.append(j)
+                answer.append(idx)
+    print(answer)
+    
+    
+    return answer
