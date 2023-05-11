a6189f10eadb0c1284d118c8d7e64ac382f9108f
Chulmin Kwak <gcm466@naver.com>
2023-05-11 10:12:29 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,18 +1,17 @@
-#압축하여 표현한 문자열 중 가장 짧은 것의 길이를 리턴하도록 솔루션 함수를 완성해라.
 def solution(s):
-    answer = len(s)
+    result = len(s)
     for step in range(1,len(s)//2 + 1):
-        cur = s[:step] 
+        cur = s[:step]
+        answer=''
         cnt = 1
-        tmp = ''
-        for j in range(step, len(s), step):
-            next = s[j:j+step]
+        for i in range(step, len(s), step):
+            next = s[i : i + step]
             if cur == next:
-                cnt+=1            
+                cnt+=1
             else:
-                tmp+= (str(cnt) + cur) if cnt>=2 else cur
+                answer += str(cnt) + cur if cnt > 1 else cur
                 cur = next
-                cnt=1
-        tmp+=str(cnt) + cur if cnt>=2 else cur #마지막 문자열 더해줌
-        answer = min(answer, len(tmp))
-    return answer
+                cnt = 1
+        answer+=str(cnt) + cur if cnt > 1 else cur
+        result = min(result, len(answer))
+    return result
