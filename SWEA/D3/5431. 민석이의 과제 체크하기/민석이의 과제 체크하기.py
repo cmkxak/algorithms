46f2aff946251399906ebad8aed40597aa05217a
Chulmin Kwak <gcm466@naver.com>
2023-05-19 17:31:39 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 383 ms, Memory: 64,368 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,19 +1,16 @@
-t= int(input())
+t = int(input())
 
-for test_case in range(1, t+1):
+for test_case in range(1, t + 1):
     stuCnt, hwStuCnt = map(int, input().split())
-    stuInfo = {}
+    stuInfo = []
 
     for i in range(1, stuCnt + 1):
-        stuInfo[i] = 1
+        stuInfo.append(str(i))
 
-    hwStuNum = list(map(int, input().split()))
+    hwStu = list(map(str, input().split()))
 
-    for i in hwStuNum:
-        stuInfo[i] = 0 #과제 했으면 0으로 바뀜
-    
-    answer = []
-    for key, val in stuInfo.items():
-        if val == 1:
-            answer.append(str(key))
-    print("#{} {}".format(test_case, str(' '.join(answer))))
+    for i in hwStu:
+        idx = stuInfo.index(i)
+        stuInfo.pop(idx)
+
+    print("#{} {}".format(test_case, str(' '.join(stuInfo))))
