5bcf3515c422db9df325d459965938b66a696a87
Chulmin Kwak <gcm466@naver.com>
2023-06-07 15:24:40 +0900

    [Silver III] Title: 파일 정리, Time: 204 ms, Memory: 48796 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,8 +1,10 @@
+import sys
+input = sys.stdin.readline
 n=int(input())
 files = []
 name_dict = {}
 for i in range(n):
-    files.append(input())
+    files.append(input().rstrip())
 for file in files:
     names = file.split('.')
     name_dict[names[1]] = name_dict.get(names[1], 0) + 1
