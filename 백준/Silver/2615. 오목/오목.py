20f7e295968a9e5fbe6a39c2cbcfd1a554ccfc7f
Chulmin Kwak <gcm466@naver.com>
2023-06-10 00:22:10 +0900

    [Silver I] Title: 오목, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,9 +1,10 @@
-import sys
-input = sys.stdin.readline
+graph = [list(map(int, input().split())) for i in range(19)]
 
 dir = [(1,0),(0,1),(-1,1), (1,1)]
-def check_direction(x,y):
-    stone = graph[x][y]
+
+
+def check_direction(x,y, stone):
+
     for k in range(4):
         cnt = 1
         nx = x + dir[k][0]
@@ -11,7 +12,7 @@ def check_direction(x,y):
 
         while 0<= nx <19 and 0<=ny<19 and graph[nx][ny] == stone:
             cnt +=1
-
+            
             if cnt == 5:
                 #육목 체크
                 if 0<= x - dir[k][0] < 19 and 0<= y - dir[k][1] < 19 and graph[x - dir[k][0]][y - dir[k][1]] == stone:
@@ -20,13 +21,15 @@ def check_direction(x,y):
                     break
                 print(stone)
                 print(x+1, y+1)
-                exit(0)
+                exit()
             nx,ny = nx + dir[k][0], ny+dir[k][1]
 
-graph = [list(map(int, input().split())) for i in range(19)]
 for x in range(19):
     for y in range(19):
-        if graph[x][y] != 0:
-            check_direction(x,y)
+        if graph[x][y] == 1:
+            check_direction(x,y,1)
+
+        elif graph[x][y] == 2:
+            check_direction(x,y,2)
 
 print(0)
