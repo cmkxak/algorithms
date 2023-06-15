65cfeadc8270a2c7731774aad118414fad966c4d
Chulmin Kwak <gcm466@naver.com>
2023-06-15 13:57:22 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,10 +1,7 @@
 def solution(food):
     answer = ''
-    repeat = 0
-    food_dict = {str(i):food[i] for i in range(1,len(food))}
-    
-    for key,val in food_dict.items():
-        answer += key * (val // 2)
-        
+    for i in range(1, len(food)):
+        cnt = food[i] // 2
+        answer += str(i) * cnt
     answer += '0' + answer[::-1]
     return answer
