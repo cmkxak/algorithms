ce8436e23be29dacb7a9a6f30e59885d0568d4c4
Chulmin Kwak <gcm466@naver.com>
2023-07-10 20:36:28 +0900

    [unrated] Title: 무인도 여행, Time: 0.24 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -1,42 +1,34 @@
 from collections import deque
 
-dx = [-1,1,0,0] #행
-dy = [0,0,-1,1] #열
-
-def bfs(x,y,maps,visited):
-    sum = int(maps[x][y])
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def bfs(i,j,maps,visited,sum):
     queue = deque()
-    queue.append((x,y))
-    
+    queue.append((i,j))   
     while queue:
-        x,y = queue.popleft()
-        
+        x,y = queue.popleft()      
         for i in range(4):
             nx = x + dx[i]
-            ny = y + dy[i]
-            
-            
-            if nx < 0 or ny < 0 or nx >= len(maps) or ny >= len(maps[0]):
-                continue
-            
-            if maps[nx][ny] != 'X':
-                if not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    sum += int(maps[nx][ny])
+            ny = y + dy[i]          
+            if 0<=nx<len(maps) and 0<=ny<len(maps[0]):
+                if maps[nx][ny] != 'X' and visited[nx][ny] != 1:
+                    sum+= int(maps[nx][ny])
                     queue.append((nx,ny))
-    return sum 
-    
+                    visited[nx][ny] = 1
+    return sum
+        
 def solution(maps):
-    answer = []
-    visited = [[False] * len(maps[0]) for i in range(len(maps))]
-    
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] != 'X':
-                if not visited[i][j]:
-                    visited[i][j] = True
-                    answer.append(bfs(i,j,maps,visited))
-    if answer:
-        return sorted(answer)
-    else:
+    answer = [] 
+    r,c = len(maps), len(maps[0])
+    visited = [[0] * c for _ in range(r)]
+    for i in range(r):
+        for j in range(c):
+            if maps[i][j] != 'X' and not visited[i][j]:
+                visited[i][j] = 1
+                sum = int(maps[i][j])
+                result = bfs(i,j,maps,visited,sum)
+                answer.append(result)
+    if not answer:
         return [-1]
+    else:
+        return sorted(answer)
