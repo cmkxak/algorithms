612a2741e06d057ac78928e38cf87d9addc5c33b
cmkxak <gcm466@naver.com>
2024-01-14 00:35:21 +0900

    [level Lv.1] Title: 명예의 전당 (1), Time: 0.37 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] result = new int[score.length];
+        
+        result[0] = score[0];
+        answer.add(score[0]);
+        
+        for (int i =1; i<score.length;i++){
+            
+            if (i > k-1){
+                answer.add(score[i]);
+                Collections.sort(answer);
+                result[i] = answer.get(i-k+1);
+            } else {
+                answer.add(score[i]);
+                Collections.sort(answer);
+                result[i] = answer.get(0);
+            }
+        }
+        
+        return result;
+    }
+}
