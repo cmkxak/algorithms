57e1cb91a712136c3a672212b5f01a0ebb63d05a
cmkxak <gcm466@naver.com>
2024-09-09 21:44:51 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 14.06 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 11.74 ms
+메모리: 80.2 MB, 시간: 14.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 14:43:19
+2024년 09월 09일 21:44:49
 
 ### 문제 설명
 
--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,21 +2,16 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        ArrayList<String> answer = new ArrayList<>();
-        ArrayList<String> arr = new ArrayList<>();
-        
-        
-        for (int i = 0; i<strings.length; i++){
-            String c = strings[i].charAt(n) + strings[i];
-            arr.add(c);
+        for(int i = 0; i < strings.length; i++){
+            strings[i] = strings[i].substring(n, n+1) + strings[i]; 
         }
         
-        Collections.sort(arr);
+        Arrays.sort(strings);
         
-        for (int i = 0; i< arr.size() ; i++){
-            answer.add(arr.get(i).substring(1));
+        for (int i = 0; i< strings.length; i++){
+            strings[i] = strings[i].substring(1);
         }
         
-        return answer.toArray(new String[0]);
+        return strings;
     }
 }
