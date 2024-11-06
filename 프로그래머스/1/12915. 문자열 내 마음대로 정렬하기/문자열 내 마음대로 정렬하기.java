20efaf3bd1957743a23ab13dea3c3a20373f4459
cmkxak <gcm466@naver.com>
2024-11-06 23:57:35 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.10 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,16 +2,22 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        for(int i = 0; i < strings.length; i++){
-            strings[i] = strings[i].substring(n, n+1) + strings[i]; 
-        }
+        String[] answer = new String[strings.length];
         
+        changeStrStartsWithNthStr(strings, n);
         Arrays.sort(strings);
         
-        for (int i = 0; i< strings.length; i++){
-            strings[i] = strings[i].substring(1);
+        for (int i = 0; i<strings.length; i++){
+            answer[i] = strings[i].substring(1);
         }
         
-        return strings;
+        return answer;
     }
+    
+    public void changeStrStartsWithNthStr(String[] arr, int n){
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = arr[i].substring(n, n+1) + arr[i];
+        }
+    }
+    
 }
