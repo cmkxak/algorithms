1836deeb0c04a3e1ad4c07b3ad6d73369554aea6
CHEOLMIN <gcm466@naver.com>
2025-05-28 00:03:04 +0900

    [level 2] Title: 삼각 달팽이, Time: 20.41 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645) 
 
 ### 성능 요약
 
-메모리: 144 MB, 시간: 19.75 ms
+메모리: 143 MB, 시간: 20.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 06일 22:28:49
+2025년 05월 28일 00:02:59
 
 ### 문제 설명
 
--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -2,46 +2,55 @@ import java.util.*;
 
 class Solution {
     
-    private static final int[]dx = {0,1,-1};
-    private static final int[]dy = {1,0,-1};
+    //방향 정의
+    private static final int dy[] = {1, 0, -1};
+    private static final int dx[] = {0, 1, -1};
+    
     public int[] solution(int n) {
+        //1. 2차원 그래프 생성
+        int graph[][] = new int[n][n];
         
-        int[][] graph = new int[n][n];
-        
-        int x =0, y = 0;
+        //2. 시작 좌표, 방향 인덱스 좌표, 초기값 생성
+        int y = 0;
+        int x = 0;
+        int dir = 0;
         int v = 1;
-        int i = 0;
-
+            
+        //3. 달팽이 이동
         while (true) {
-            
+            //4. 이동 처리 및 다음 방향 선정
             graph[y][x] = v++;
-            
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if (nx == n || ny == n ||  nx == -1 || ny == -1 || graph[ny][nx] != 0) {
-                i = (i + 1) % 3;
-                nx = x + dx[i];
-                ny = y + dy[i];
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+        
+            //4.1 새로 이동한 방향이 범위가 벗어나거나 이미 방문한 경우
+            if (nx == n || ny == n || nx == -1 || ny == -1 || graph[ny][nx] != 0) {
+                //4.1.1 방향 전환
+                dir = (dir + 1) % 3;
+                ny = y + dy[dir];
+                nx = x + dx[dir];
                 
-                if (nx == n || ny == n || nx == -1 || ny == -1 || graph[ny][nx] != 0) {
-                    break;
-                }
+                //4.1.2 전환된 방향이 이동할 수 있는지 검증
+                if (nx == n || ny == n || nx == -1 || ny == -1 || graph[ny][nx] != 0) break;
             }
+            
+            //5. 이동된 좌표로 초기화
             y = ny;
             x = nx;
         }
         
-        int[] answer = new int[v - 1];
+        //6. 1차원 결과 배열 생성
+        int answer[] = new int[v-1];
         
         int idx = 0;
-        for (int k =0; k<n; k++){
-            for (int j = 0; j <= k; j++){
-                answer[idx++] = graph[k][j]; 
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <=i; j++){
+                //7. 달팽이 이동을 통해 채워진 값 -> 정답 배열에 할당
+                answer[idx++] = graph[i][j];
             }
         }
         
-        
+        //8. 정답 리턴
         return answer;
     }
 }
