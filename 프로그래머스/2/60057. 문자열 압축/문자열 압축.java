6fa592c1b4f387a79c8fd8001b16a2ab43ada0b1
CHEOLMIN <gcm466@naver.com>
2025-05-29 23:07:01 +0900

    [level 2] Title: 문자열 압축, Time: 16.48 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,38 +1,42 @@
-import java.util.*; 
+import java.util.*;
 
-class Solution {    
+class Solution {
     public int solution(String s) {
-        int answer = s.length(); //최대 길이로 초기화
+        int answer = s.length();
+        int n = s.length();
         
-        for (int i = 1; i<=s.length() / 2; i++){    
+        for (int i = 1; i <= n / 2; i++){
             StringBuilder sb = new StringBuilder();
-            
-            String cur = s.substring(0, i);
-            int cnt = 1;
+            int prefixCnt = 1;
+            String prefix = s.substring(0, i);
             String next = "";
             
-            for (int j = i; j< s.length(); j+=i){
-                if (j + i > s.length())
-                    next = s.substring(j, s.length());
-                else
-                    next = s.substring(j, j+i);
-                if (cur.equals(next)){
-                    cnt += 1;
+            for (int j = i; j < n; j+=i){
+                if (j + i > n) {
+                    next = s.substring(j, n);    
                 } else {
-                    if (cnt > 1)
-                        sb.append(cnt + cur);
-                    else
-                        sb.append(cur);
-                    cur = next;
-                    cnt = 1;
+                    next = s.substring(j, j+i);
+                }
+            
+                if (prefix.equals(next)) {
+                    prefixCnt++;
+                } else {
+                    if (prefixCnt > 1) {
+                        sb.append(prefixCnt).append(prefix);
+                    } else {
+                        sb.append(prefix);
+                    }
+                    prefix = next;
+                    prefixCnt = 1;
                 }
             }
-            if (cnt > 1)
-                sb.append(cnt + cur);
+            if (prefixCnt > 1)
+                sb.append(prefixCnt + prefix);
             else
-                sb.append(cur);
+                sb.append(prefix);
             answer = Math.min(answer, sb.toString().length());
         }
+        
         return answer;
-    }  
+    }
 }
