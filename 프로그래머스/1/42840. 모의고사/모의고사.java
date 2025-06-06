e43088a471e5cdefb781f8f599998ef6ffd4c6b9
CHEOLMIN <gcm466@naver.com>
2025-06-06 18:16:56 +0900

    [level 1] Title: 모의고사, Time: 1.17 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,41 +1,59 @@
 import java.util.*;
 
 class Solution {
-    private static int MAX_SCORE = 0;
-    private static final int[] scoreBoard = new int[3];
-    private ArrayList<Integer> answer = new ArrayList<>();
-    private int idx = 0;
+    private static int MAX = 0;
+    private static int[] studentScores = new int[3];
     
     public int[] solution(int[] answers) {
+        int[] stu1 = {1,2,3,4,5};
+        int[] stu2 = {2,1,2,3,2,4,2,5};
+        int[] stu3 = {3,3,1,1,2,2,4,4,5,5};
+                    
+        int stu1Score = getStuScore(answers, stu1, 0);
+        int stu2Score = getStuScore(answers, stu2, 1);
+        int stu3Score = getStuScore(answers, stu3, 2);
         
-        int[] stu1 = new int[]{1,2,3,4,5};
-        int[] stu2 = new int[]{2,1,2,3,2,4,2,5};
-        int[] stu3 = new int[]{3,3,1,1,2,2,4,4,5,5};
-      
-        getEachStuScore(stu1, answers, scoreBoard);
-        getEachStuScore(stu2, answers, scoreBoard);
-        getEachStuScore(stu3, answers, scoreBoard);
-        setMaxScore(scoreBoard);
+        updateMaxScore(studentScores);
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int maxScoreStudentCnt = getMaxScoreStudentCnt(studentScores);
+
+        return getMaxScoreStudentList(studentScores, maxScoreStudentCnt);
     }
     
-    public void getEachStuScore(int[] stuScore, int[] answers, int[] board){
+    private void updateMaxScore(int[] studentScores){
+        for (int i = 0; i < studentScores.length; i++){
+            MAX = Math.max(MAX, studentScores[i]);
+        }
+    }
+    
+    private int getStuScore(int[] answers, int[] studentAnswers, int studentIdx){
         int score = 0;
-        
+        int n = studentAnswers.length;
         for (int i = 0; i < answers.length; i++){
-            if (stuScore[i % stuScore.length] == answers[i])
+            if (answers[i] == studentAnswers[i % n]) {
                 score++;
+            }
         }
-        board[idx++] = score;
-        MAX_SCORE = Math.max(MAX_SCORE, score);
+        studentScores[studentIdx] = score;
+        return score;
     }
     
-    public void setMaxScore(int[] board){
-        for (int i = 0; i< board.length; i++){
-            if (board[i] == MAX_SCORE) 
-                answer.add(i+1);
+    private int getMaxScoreStudentCnt(int[] studentScores){
+        int size = 0;
+        for (int i = 0; i < studentScores.length; i++){
+            if (studentScores[i] == MAX) size++;
         }
+        return size;
     }
-     
+    
+     private int[] getMaxScoreStudentList(int[] studentScores, int size){
+        int[] answer = new int[size];
+        int idx = 0;
+        for (int i = 0; i < studentScores.length; i++){
+            if (studentScores[i] == MAX){
+                answer[idx++] = i + 1;
+            }
+        }
+        return answer;
+    }
 }
